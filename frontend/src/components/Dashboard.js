32104cca9904cb3a5b548ee135c9d680bebb96b1
Kirubakaran Periyasamy <88470706+kiruba11k@users.noreply.github.com>
2026-03-03 10:56:42 +0530

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -7,31 +7,12 @@ return (
 <div className="cards">
 
 
-// <div className="card">
 
-// <h3>Total Leads</h3>
-
-// <p>1,284</p>
-
-// </div>
 
 
 <div className="card">
 
-// <h3>Success Rate</h3>
 
-// <p>82%</p>
-
-// </div>
-
-
-// <div className="card">
-
-// <h3>Accuracy</h3>
-
-// <p>91%</p>
-
-// </div>
 
 
 </div>
